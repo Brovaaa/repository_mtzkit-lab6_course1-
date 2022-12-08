62ca9a06bd06eed930c59d9415186629332bcf65
Boiko Volodymyr <brova.only.work@gmail.com>
2022-12-08 06:17:02 +0200

    First git commit...Створено найпростіший консольний додаток

---
+++ mtzkit_github_lab_6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace mtzkit_github_lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
